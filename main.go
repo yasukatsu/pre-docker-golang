9b5c33fe9bd7828a3bf346c1431e81be55f97843
Yasukastsu.A <handbuuuu@gmail.com>
2019-11-30 02:07:20 +0900

    ijiiji

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+)
+
+type hoge struct {
+	Status  int    `json:"status"`
+	Results string `json:"results"`
+}
 
 func main() {
 	fmt.Println("test")
+	http.HandleFunc("/", rootHandler)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Printf("%v\n", string(dump))
+
+	w.Header().Set("Content-Type", "application/json")
 }
